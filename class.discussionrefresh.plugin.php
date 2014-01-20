c8cade7bce71febe51f41bf418be14f36958c750
R-J <robin.jurinka@gmx.net>
2014-01-20 22:17:00 +0100

    Update class.discussionrefresh.plugin.php

--- class.discussionrefresh.plugin.php
+++ class.discussionrefresh.plugin.php
@@ -5,39 +5,27 @@ $PluginInfo['DiscussionRefresh'] = array(
    'Description' => 'Shows comments that have been posted to a dsicussion between opening it and pressing preview button',
    'Version' => '0.1',
    'RequiredApplications' => array('Vanilla' => '2.0.18'), 
-   'HasLocale' => FALSE,
-   'Author' => "Robin",
-   'RegisterPermissions' => FALSE
+   'Author' => "Robin"
 );
 
-class DiscussionRefreshPlugin extends Gdn_Plugin {
 /*
-    php: hook into some discussion event to insert a javascript file
-ok    
-    js: get the LastCommentID from current form and store it somehow
-ok. somehow, by now...    
-    js: "hook" (attach an event in js-speak?) to the preview button
-    
-    js: if preview is pressed, call a php function "GetNewComments(DiscussionID, LastCommentID)" (request /plugin/pluginname/getnewcommentids/DiscussionID/LastCommentID).
-    php: GetNewComments(LastCommentID): NewComments = select CommentID from Discussion where DiscussionID = $DiscussionID and LastCommentID > $LastCommentID. Return CommentModel::Get(NewComments) and max(NewComments)
     js: LastCommentID = max(NewComments)
     js: insert html for new comments
 */
+class DiscussionRefreshPlugin extends Gdn_Plugin {
 
-
-   // insert js into discussion controller
+   // insert js at the end of discussion controller
    public function DiscussionController_AfterBody_Handler($Sender) {
       echo '<script src="/plugins/DiscussionRefresh/js/discussionrefresh.js"></script>';
    }
    
-   // route all requests to /plugin/discussionrefreh/... to function Controller_...
+   // route all requests to /discussion/discussionrefreh/... to function Controller_...
    public function DiscussionController_DiscussionRefresh_Create($Sender, $Args) {
       return $this->Dispatch($Sender);
    }
    
-   // default action is to return comments of a given discussion
+   //get all comments of a discussion and return only the latest
    public function Controller_Index($Sender) {
-      //get all comments of a discussion and return only the latest
       $Args = $Sender->RequestArgs;   
 
       // no request, no result
@@ -50,25 +38,30 @@ ok. somehow, by now...
       // check for proper discussionid
       if (!is_numeric($DiscussionID))
          return FALSE;
-
-      $CommentModel = new CommentModel();
-      $Comments = $CommentModel->Get($DiscussionID)->Result();
+      
+      // check permissions
+      $DiscussionModel = new DiscussionModel();
+      $Discussion = $DiscussionModel->GetID($DiscussionID);
+      $Sender->Permission('Vanilla.Discussions.View', TRUE, 'Category', $Discussion->PermissionCategoryID);
 
       // if no comment id is given, return all comments
       if (!is_numeric($CommentID))
          $CommentID = 0;
 
-decho($Comments);
-      foreach ($Comments as $Index => $Comment) {
-         if ($Comment->CommentID <= $CommentID) {
-            unset($Comments[$Index]);
+      $CommentModel = new CommentModel();
+      $Comments = $CommentModel->Get($DiscussionID);
+
+      // keep only new comments
+      $CommentsFiltered = array();
+      foreach ($Comments as $Comment) {
+         if ($Comment->CommentID > $CommentID) {
+            $CommentsFiltered[] = $Comment;
          }
       }
-decho($Comments);
+      $Comments->ImportDataset($CommentsFiltered);
 
+      // render all comments
       $Sender->SetData('CommentData', $Comments, TRUE);
-      //$Sender->DeliveryType = 'VIEW';
-      //&DeliveryMethod=JSON
       $Sender->Render($Sender->FetchViewLocation('comments', 'discussion'));
    }
 }
