a545803625df8802d38ee0161c9b6afd71dac5de
R-J <robin.jurinka@gmx.net>
2014-07-22 14:39:28 +0200

    Update class.discussionrefresh.plugin.php

--- class.discussionrefresh.plugin.php
+++ class.discussionrefresh.plugin.php
@@ -1,67 +1,79 @@
-<?php if (!defined('APPLICATION')) exit();
+<?php defined('APPLICATION') or die();
 
 $PluginInfo['DiscussionRefresh'] = array(
-   'Name' => 'Discussion Refresh',
-   'Description' => 'Shows comments that have been posted to a dsicussion between opening it and pressing preview button',
-   'Version' => '0.1',
-   'RequiredApplications' => array('Vanilla' => '2.0.18'), 
-   'Author' => "Robin"
+    'Name' => 'Discussion Refresh',
+    'Description' => 'Loads new comments when either "Preview", "Edit", "Save Draft" or "Post Comment" button is pressed.',
+    'Version' => '0.1',
+    'RequiredApplications' => array('Vanilla' => '2.1'),
+    'MobileFriendly' => true,
+    'Author' => 'Robin Jurinka',
+    'License' => 'MIT'
 );
 
-/*
-    js: LastCommentID = max(NewComments)
-    js: insert html for new comments
-*/
+/**
+ * Loads new comments when "Preview", "Edit", "Save Draft" or "Post Comment" button is pressed.
+ *
+ * @package DiscussionRefresh
+ * @author Robin Jurinka
+ * @license MIT
+ */
 class DiscussionRefreshPlugin extends Gdn_Plugin {
+    /**
+     * Appends JavaScript to a discussion.
+     *
+     * @param object $Sender DiscussionController.
+     * @package DiscussionRefresh
+     * @since 0.1
+     */
+    public function discussionController_render_before($Sender) {
+        $Sender->AddJsFile('discussionrefresh.js', 'plugins/DiscussionRefresh');
+    }
 
-   // insert js at the end of discussion controller
-   public function DiscussionController_AfterBody_Handler($Sender) {
-      echo '<script src="/plugins/DiscussionRefresh/js/discussionrefresh.js"></script>';
-   }
-   
-   // route all requests to /discussion/discussionrefreh/... to function Controller_...
-   public function DiscussionController_DiscussionRefresh_Create($Sender, $Args) {
-      return $this->Dispatch($Sender);
-   }
-   
-   //get all comments of a discussion and return only the latest
-   public function Controller_Index($Sender) {
-      $Args = $Sender->RequestArgs;   
+    /**
+     * Return view for new comments and LastCommentID for further reference.
+     *
+     * @param object $Sender DiscussionController.
+     * @param array $Args DiscussionID and LastCommentID.
+     * @package DiscussionRefresh
+     * @since 0.1
+     */
+    public function discussionController_discussionRefresh_create($Sender, $Args) {
+        $DiscussionID = $Args[0] + 0;
+        $LastCommentID = $Args[1] + 0;
 
-      // no request, no result
-      if (count($Args) == 0)
-         return FALSE;
-     
-      $DiscussionID = $Args[0];
-      $CommentID = $Args[1];
+        // don't forget permission check!
+        $DiscussionModel = new DiscussionModel();
+        $Discussion = $DiscussionModel->GetID($DiscussionID);
+        $Sender->Permission('Vanilla.Discussions.View', true, 'Category', $Discussion->PermissionCategoryID);
 
-      // check for proper discussionid
-      if (!is_numeric($DiscussionID))
-         return FALSE;
-      
-      // check permissions
-      $DiscussionModel = new DiscussionModel();
-      $Discussion = $DiscussionModel->GetID($DiscussionID);
-      $Sender->Permission('Vanilla.Discussions.View', TRUE, 'Category', $Discussion->PermissionCategoryID);
+        // nothing to do if there are no new comments
+        if ($Discussion->LastCommentID <= $LastCommentID) {
+            return;
+        }
 
-      // if no comment id is given, return all comments
-      if (!is_numeric($CommentID))
-         $CommentID = 0;
+        // build comment views
+        $HtmlOut = '';
+        $ItemCount = 0;
+        $Session = Gdn::Session();
+        // include view for comments
+        include_once(Gdn::Controller()->FetchViewLocation('helper_functions', 'Discussion', 'Vanilla'));
+        $CommentModel = new CommentModel();
+        $Comments = $CommentModel->Get($DiscussionID);
 
-      $CommentModel = new CommentModel();
-      $Comments = $CommentModel->Get($DiscussionID);
+        foreach ($Comments as $Comment) {
+            $CommentID = $Comment->CommentID;
+            ++$ItemCount;
+            if ($CommentID > $LastCommentID) {
+                ob_start();
+                WriteComment($Comment, $this, $Session, $ItemCount);
+                $HtmlOut .= ob_get_clean();
+            }
+        }
 
-      // keep only new comments
-      $CommentsFiltered = array();
-      foreach ($Comments as $Comment) {
-         if ($Comment->CommentID > $CommentID) {
-            $CommentsFiltered[] = $Comment;
-         }
-      }
-      $Comments->ImportDataset($CommentsFiltered);
+        // update UserDiscussion table
+        $CountComments = $Discussion->CountComments;
+        $CommentModel->SetWatch($Discussion, $CountComments, $CountComments, $CountComments);
 
-      // render all comments
-      $Sender->SetData('CommentData', $Comments, TRUE);
-      $Sender->Render($Sender->FetchViewLocation('comments', 'discussion'));
-   }
+        echo(json_encode(array('LastCommentID' => $CommentID, 'UnreadComments' => $HtmlOut)));
+    }
 }
